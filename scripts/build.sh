f00ff61b92e17e6dc4a0733776963b9436a105eb
m43islam <m43islam@uwaterloo.ca>
2022-06-26 12:49:46 -0400

    Boilerplate lexyacc app

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,12 @@
-ocamlopt -o bin/giraph -I src src/codeStream.ml src/scanner.ml src/generator.ml src/compiler.ml
-rm -rf src/**.cmi src/**.cmx src/**.o
+ocamllex -o src/lexyacc/lexer.ml src/lexyacc/definitions/lexer.mll
+ocamlyacc -b src/lexyacc/parser src/lexyacc/definitions/parser.mly
+
+cd src/lexyacc
+ocamlc -c parser.mli
+ocamlc -c lexer.ml
+ocamlc -c parser.ml
+ocamlc -c calc.ml
+ocamlc -o ../../bin/giraph lexer.cmo parser.cmo calc.cmo
+
+cd ../..
+rm -rf src/lexyacc/**.cmi src/lexyacc/**.cmx src/lexyacc/**.cmo
